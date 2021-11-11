1fe4b4648de2c27cd70b26e3ab5ef19cc7c995ea
Rohit Kumar Singh <rohitt.singh15@gmail.com>
2021-11-11 17:20:21 +0530

    setting port

--- src/test/java/com/developer/msscbeer/inventory/MsscBeerInventoryServiceApplicationTests.java
+++ src/test/java/com/developer/msscbeer/inventory/MsscBeerInventoryServiceApplicationTests.java
@@ -1,9 +1,10 @@
 package com.developer.msscbeer.inventory;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@WebMvcTest(MsscBeerInventoryServiceApplication.class)
 class MsscBeerInventoryServiceApplicationTests {
 
 	@Test
